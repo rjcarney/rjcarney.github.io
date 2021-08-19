6863a73825cb9154996115b11429d1863daed641
rjcarney <33989142+rjcarney@users.noreply.github.com>
2021-08-18 20:40:49 -0400

    Parallax Image, Recoloring, and Contact Form

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $service = $_POST['service'] . " - Job Inquiry";
+  $description = $_POST['description'];
+  $outreach = $_POST['outreach'];
+
+  $message = $description . "\n" . $name . "\n" . $email . "\n" . $outreach;
+
+  mail("rcarney.rcweb@gmail.com", $service, $message);
+
+  header("Location: http://www.rcweb.studio/index.html");
+?>
